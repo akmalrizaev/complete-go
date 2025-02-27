cf2c9da69afdde0de2707474adf9106de5e01193
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-02-27 11:47:05 +0500

    Functions: Return Values & Variable Scope

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -5,8 +5,10 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
+
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
@@ -21,8 +23,10 @@ func main() {
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	// outputs information
 	// fmt.Println("Future Value", futureValue)
@@ -32,4 +36,10 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
