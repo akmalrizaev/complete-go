09dc58a3798d273f04edfa7f6f3bf71ed39e7aba
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-06 13:41:01 +0500

    Structs, Packages & Exports

--- structs/structs.go
+++ structs/structs.go
@@ -1,49 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("First name, last name, birthdate are required")
-	}
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser = &user.User{
+		FirstName: "Max",
+	}
+
+	// appUser, err := newUser(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println(err)
---
+++ structs/user/user.go
@@ -0,0 +1,37 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	FirstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+
+}
+
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("First name, last name, birthdate are required")
+	}
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
