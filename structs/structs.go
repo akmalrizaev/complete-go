1449d2576e2e2c0d70259b4701c348a1eb60364f
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-06 09:01:36 +0500

    Structs & Methods

--- structs/structs.go
+++ structs/structs.go
@@ -12,6 +12,11 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -28,12 +33,9 @@ func main() {
 
 	// ... do something awesome with that gathered data!
 
-	outputUserDetails(&appUser)
-}
+	// outputUserDetails(&appUser)
 
-func outputUserDetails(u *user) {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
