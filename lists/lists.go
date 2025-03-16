017750a9b0368dc0bcb88732a6fd141ecd4f46ce
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-16 09:38:43 +0500

    Working with Arrays

--- lists/lists.go
+++ lists/lists.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string = [4]string{"A Book"}
 	prices := [4]float64{10.99, 9.99, 5.99, 20.00}
 	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
+
+	fmt.Println(productNames)
+	fmt.Println(prices[2])
 }
