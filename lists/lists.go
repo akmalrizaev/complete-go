66b88d8e93215032e7efc879088ea9e44e3b7a94
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-16 16:14:17 +0500

    Unpacking List Values

--- lists/lists.go
+++ lists/lists.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 5.99, 20.00}
+	prices := []float64{10.99, 9.99, 5.99, 20.00}
 	fmt.Println(prices)
 
 	productNames[2] = "A Carpet"
@@ -14,4 +14,8 @@ func main() {
 
 	featuredPrices := prices[1:3]
 	fmt.Println(featuredPrices)
+
+	discountPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discountPrices...)
+	fmt.Println(prices)
 }
